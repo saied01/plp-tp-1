fdba4d1671c3f7946b618a1725b608b01faac4d3
felipeuba <165933197+felipeuba@users.noreply.github.com>
2025-04-09 16:36:04 -0300

    tests, todos los ejs cubiertos

--- Main.hs
+++ Main.hs
@@ -13,10 +13,10 @@ allTests =
      [ "Ejercicio 2" ~: testsEj2,
        "Ejercicio 3" ~: testsEj3,
        "Ejercicio 4" ~: testsEj4,
-       "Ejercicio 6" ~: testsEj6
-    --   "Ejercicio 7" ~: testsEj7,
-    --  "Ejercicio 8" ~: testsEj8
-    --   "Ejercicio 9" ~: testsEj9
+       "Ejercicio 6" ~: testsEj6,
+       "Ejercicio 7" ~: testsEj7,
+       "Ejercicio 8" ~: testsEj8,
+       "Ejercicio 9" ~: testsEj9
     ]
 
 testsEj2 :: Test
@@ -99,19 +99,62 @@ testsEj7 =
     [ mostrar (intercalar (texto ", ") []) ~?= "",
       mostrar (intercalar (texto ", ") [a, b, c]) ~?= "a, b, c",
       mostrar (entreLlaves []) ~?= "{ }",
-      mostrar (entreLlaves [a, b, c]) ~?= "{\n  a,\n  b,\n  c\n}"
+      mostrar (entreLlaves [a, b, c]) ~?= "{\n  a,\n  b,\n  c\n}",
+
+      -- un solo elemento en `intercalar`
+      mostrar (intercalar (texto ", ") [a]) ~?= "a",
+
+      -- un solo elemento en `entreLlaves`
+      mostrar (entreLlaves [a]) ~?= "{\n  a\n}",
+
+      -- diferente separador en `intercalar`
+      mostrar (intercalar (texto " | ") [a, b, c]) ~?= "a | b | c",
+
+      -- dos elementos en `entreLlaves`
+      mostrar (entreLlaves [b, c]) ~?= "{\n  b,\n  c\n}"
     ]
 
 testsEj8 :: Test
 testsEj8 =
   test
-    [ mostrar (aplanar (a <+> linea <+> b <+> linea <+> c)) ~?= "a b c"
+    [
+      mostrar (aplanar (a <+> linea <+> b <+> linea <+> c)) ~?= "a b c",
+
+      -- un solo elemento (sin líneas para aplanar)
+      mostrar (aplanar a) ~?= "a",
+
+      -- una línea e indentación
+      mostrar (aplanar (a <+> indentar 2 (linea <+> b))) ~?= "a b",
+
+      -- múltiples niveles de indentación
+      mostrar (aplanar (a <+> indentar 4 (linea <+> b <+> indentar 2 (linea <+> c)))) ~?= "a b c",
+
+      -- documentos vacíos
+      mostrar (aplanar (texto "" <+> linea <+> texto "")) ~?= "",
+
+      -- varios elementos separados por líneas
+      mostrar (aplanar (texto "x" <+> linea <+> texto "y" <+> linea <+> texto "z")) ~?= "x y z"
     ]
+
 
 testsEj9 :: Test
 testsEj9 =
   test
     [ mostrar (pponADoc pericles) ~?= "{ \"nombre\": \"Pericles\", \"edad\": 30 }",
+
+      -- un objeto compuesto de dos elementos
       mostrar (pponADoc addams) ~?= "{\n  \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n  \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n}",
-      mostrar (pponADoc familias) ~?= "{\n  \"Addams\": {\n    \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n    \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n  }\n}"
+
+      -- un objeto compuesto con nombres como claves
+      mostrar (pponADoc familias) ~?= "{\n  \"Addams\": {\n    \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n    \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n  }\n}",
+
+      -- objeto vacío
+      mostrar (pponADoc (ObjetoPP [])) ~?= "{ }",
+
+      -- claves y valores más complejos
+      mostrar (pponADoc (ObjetoPP [("clave1", TextoPP "valor1"), ("clave2", IntPP 42)])) ~?= "{\n  \"clave1\": \"valor1\",\n  \"clave2\": 42\n}",
+
+      -- PPON anidado
+      mostrar (pponADoc (ObjetoPP [("anidado", ObjetoPP [("subclave", TextoPP "subvalor")])])) ~?=  "{\n  \"anidado\": { \"subclave\": \"subvalor\" }\n}"
     ]
+
